160b057a77037bc2517519253a1353edcb3c36bc
PauloCoder <williams56@hotmail.fr>
2023-12-28 20:49:48 +0100

    Test d'intégration pour la méthode publishUpdateLibraryEvent

--- src/test/integ/com/learnkafka/consumer/LibraryEventsConsumerIntegrationTest.java
+++ src/test/integ/com/learnkafka/consumer/LibraryEventsConsumerIntegrationTest.java
@@ -1,6 +1,9 @@
 package com.learnkafka.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.entity.Book;
+import com.learnkafka.entity.LibraryEvent;
 import com.learnkafka.entity.LibraryEventType;
 import com.learnkafka.jpa.LibraryEventsRepository;
 import com.learnkafka.service.LibraryEventsService;
@@ -32,9 +35,11 @@ import java.util.concurrent.TimeUnit;
         "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}"
 })
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class LibraryEventsConsumerIntegrationTest {
 
+    @Autowired
+    private ObjectMapper objectMapper;
     @Resource
     private EmbeddedKafkaBroker embeddedKafkaBroker;
 
@@ -92,6 +97,54 @@ class LibraryEventsConsumerIntegrationTest {
                    Assertions.assertThat(libraryEvent.getLibraryEventId()).isNotNull();
                    Assertions.assertThat(libraryEvent.getBook().getBookId()).isEqualTo(456);
                    Assertions.assertThat(libraryEvent.getLibraryEventType()).isEqualTo(LibraryEventType.NEW);
+                });
+
+    }
+
+    @Test
+    void testPublishUpdateLibraryEvents() throws JsonProcessingException, ExecutionException, InterruptedException {
+
+        // ARRANGE
+        String json = """
+                {
+                    "libraryEventId": null,
+                    "libraryEventType": "NEW",
+                    "book": {
+                        "bookId": 456,
+                        "bookName": "Kafka Using Spring Boot",
+                        "bookAuthor": "Dilip"
+                    }
+                }
+                """;
+        LibraryEvent libraryEvent = objectMapper.readValue(json, LibraryEvent.class);
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+
+        Book updatedBook = Book.builder()
+                .bookId(456)
+                .bookName("Kafka Using Spring Boot 2.X")
+                .bookAuthor("Antonio")
+                .build();
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+        libraryEvent.setBook(updatedBook);
+        String updatedJson = objectMapper.writeValueAsString(libraryEvent);
+
+        // ACT
+        kafkaTemplate.send("library-events-test", libraryEvent.getLibraryEventId(), updatedJson)
+                .get();
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await(3, TimeUnit.SECONDS);
+
+        // ASSERT
+
+        Assertions.assertThat(libraryEventsRepository.findById(libraryEvent.getLibraryEventId()))
+                .isNotNull()
+                .get()
+                .satisfies(libraryEventVerif -> {
+                    Assertions.assertThat(libraryEventVerif.getLibraryEventId()).isNotNull();
+                    Assertions.assertThat(libraryEventVerif.getBook().getBookId()).isEqualTo(456);
+                    Assertions.assertThat(libraryEventVerif.getLibraryEventType()).isEqualTo(LibraryEventType.UPDATE);
+                    Assertions.assertThat(libraryEventVerif.getBook().getBookName()).isEqualTo("Kafka Using Spring Boot 2.X");
                 });
 
     }
