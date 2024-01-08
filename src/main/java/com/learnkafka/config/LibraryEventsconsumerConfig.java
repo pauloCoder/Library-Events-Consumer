0152e638149f318239d86a0d9cae62d854360eec
PauloCoder <williams56@hotmail.fr>
2024-01-08 01:20:02 +0100

    Ajout du Retryistener pour logger l'exception et le nombre de tentatives d'erreur

--- src/main/java/com/learnkafka/config/LibraryEventsconsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsconsumerConfig.java
@@ -1,6 +1,7 @@
 package com.learnkafka.config;
 
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
+@Slf4j
 @Setter
 @Configuration
 @EnableKafka
@@ -27,6 +29,9 @@ public class LibraryEventsconsumerConfig {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         DefaultErrorHandler defaultErrorHandler = new DefaultErrorHandler(
                 new FixedBackOff(backOffInterval, backOffMaxAttempts - 1)
+        );
+        defaultErrorHandler.setRetryListeners(
+                (failedRecord, ex, deliveryAttempt) -> log.error("Failed record in Retry Listener, Exception : {}, deliveryAttempt : {}", ex.getMessage(), deliveryAttempt)
         );
         configurer.configure(factory, kafkaConsumerFactory);
         factory.setConcurrency(3);
